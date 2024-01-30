c4e51917363de685b4f91a52215f1a90b98cf229
Teddy <a1848250995@163.com>
2024-01-29 20:22:45 -0600

    initial

---
+++ trial1.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2019 Computer Vision Center (CVC) at the Universitat Autonoma de
+# Barcelona (UAB).
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+import glob
+import os
+import sys
+
+import carla
+
+import random
+import time
+
+
+def main():
+    actor_list = []
+
+    try:
+        client = carla.Client('localhost', 2000)
+        client.set_timeout(2.0)
+
+        # Once we have a client we can retrieve the world that is currently
+        # running.
+        world = client.get_world()
+        # change map
+        world = client.load_world('Town04')
+
+        # The world contains the list blueprints that we can use for adding new
+        # actors into the simulation.
+        blueprint_library = world.get_blueprint_library()
+
+        # Now let's filter all the blueprints of type 'vehicle' and choose one
+        # at random.
+        bp = random.choice(blueprint_library.filter('vehicle'))
+
+        # A blueprint contains the list of attributes that define a vehicle's
+        # instance, we can read them and modify some of them. For instance,
+        # let's randomize its color.
+        if bp.has_attribute('color'):
+            color = random.choice(bp.get_attribute('color').recommended_values)
+            bp.set_attribute('color', color)
+
+        # Now we need to give an initial transform to the vehicle. We choose a
+        # random transform from the list of recommended spawn points of the map.
+        transform = random.choice(world.get_map().get_spawn_points())
+
+        # So let's tell the world to spawn the vehicle.
+        vehicle = world.spawn_actor(bp, transform)
+
+        actor_list.append(vehicle)
+        print('created %s' % vehicle.type_id)
+
+        # Let's put the vehicle to drive around.
+        vehicle.set_autopilot(True)
+
+        transform.location += carla.Location(x=40, y=-3.2)
+        transform.rotation.yaw = -180.0
+        for _ in range(0, 10):
+            # transform.location.x += 8.0
+            transform = random.choice(world.get_map().get_spawn_points())
+
+
+            bp = random.choice(blueprint_library.filter('vehicle'))
+
+            # This time we are using try_spawn_actor. If the spot is already
+            # occupied by another object, the function will return None.
+            npc = world.try_spawn_actor(bp, transform)
+            if npc is not None:
+                actor_list.append(npc)
+                npc.set_autopilot(True)
+                print('created %s' % npc.type_id)
+
+        time.sleep(60)
+
+    finally:
+
+        print('destroying actors')
+        client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
+        print('done.')
+
+    
+
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('\ndone.')
